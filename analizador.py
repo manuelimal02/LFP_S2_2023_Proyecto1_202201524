2623410a559acdfdbea057f6e5c4663083555ac0
manuelimal02 <manuelimal02@gmail.com>
2023-09-12 14:04:28 -0600

    Cambios Analizador y Manual de Usuario

--- analizador.py
+++ analizador.py
@@ -5,7 +5,7 @@ from clases_abstractas.numero import *
 from errores.generar_errores import *
 import os
 
-#Palabras
+#PALABRAS RESERVADAS - LEXEMAS
 reserved = {
     'ROPERACIONES'      : 'operaciones',
     'ROPERACION'        : 'operacion',
@@ -201,7 +201,7 @@ def graficar():
     titulo = lista_datos_graphviz[0]
     dot = 'digraph grafo{\n'
     for i in range(len(instrucciones)):
-        dot += separar(i, 0, '', instrucciones[i])
+        dot += configuracion_nodo(i, 0, '', instrucciones[i])
     dot += f'''
     labelloc = "t"
     label = "{titulo}"
@@ -213,7 +213,7 @@ def generar_grafica(nombre_grafica):
     nombre = nombre_grafica+".dot"
     with open(nombre, 'w') as f:
         f.write(graficar())
-    os.system(f'dot -Tpng {nombre} -o {nombre_grafica}.png')
+    os.system(f'dot -Tpdf {nombre} -o {nombre_grafica}.pdf')
 
 def limpiar_lista():
     instrucciones.clear()
@@ -224,91 +224,82 @@ def limpiar_lista_errores():
     lista_errores.clear()
     n_linea = 1
 
-def separar(i, id, etiqueta, objeto):
+def configuracion_nodo(i, id, etiqueta, objeto):
     global lista_datos_graphviz
-    colorFondo = lista_datos_graphviz[1]
-    colorFuente = lista_datos_graphviz[2]
-    forma = lista_datos_graphviz[3]
-    #Colores
-    rojo='red'
-    amarillo='yellow'
-    azul='blue'
-    morado='purple'
-    naranja='orange'
-    verde='green'
-    negro='black'
-    blanco='white'
+    color_fondo_nodo = lista_datos_graphviz[1]
+    color_fuente_nodo= lista_datos_graphviz[2]
+    forma_nodo = lista_datos_graphviz[3]
     #Fondo
-    if colorFondo=="rojo":
-        colorFondo=rojo
-    elif colorFondo=="amarillo":
-        colorFondo=amarillo
-    elif colorFondo=="azul":
-        colorFondo=azul
-    elif colorFondo=="morado":
-        colorFondo=morado
-    elif colorFondo=="naranja" or colorFondo == "anaranjado":
-        colorFondo=naranja
-    elif colorFondo=="verde":
-        colorFondo=verde
-    elif colorFondo=="blanco":
-        colorFondo = negro
-    elif colorFondo=="negro":
-        colorFondo=blanco
+    if color_fondo_nodo=="rojo" or color_fondo_nodo=="red":
+        color_fondo_nodo="red"
+    elif color_fondo_nodo=="amarillo" or color_fondo_nodo=="yellow":
+        color_fondo_nodo="yellow"
+    elif color_fondo_nodo=="azul" or color_fondo_nodo=="blue":
+        color_fondo_nodo="blue"
+    elif color_fondo_nodo=="morado" or  color_fondo_nodo=="purple":
+        color_fondo_nodo="purple"
+    elif color_fondo_nodo=="naranja" or color_fondo_nodo=="anaranjado" or color_fondo_nodo=="orange":
+        color_fondo_nodo="orange"
+    elif color_fondo_nodo=="verde" or color_fondo_nodo=="green":
+        color_fondo_nodo="green"
+    elif color_fondo_nodo=="blanco" or color_fondo_nodo=="white":
+        color_fondo_nodo="white"
+    elif color_fondo_nodo=="negro" or  color_fondo_nodo=="black":
+        color_fondo_nodo="black"
     else:
-        colorFondo = "grey"
+        color_fondo_nodo = "white"
     #Fuente
-    if colorFuente == "rojo":
-        colorFuente = rojo
-    elif colorFuente == "amarillo":
-        colorFuente = amarillo
-    elif colorFuente == "azul":
-        colorFuente = azul
-    elif colorFuente == "morado":
-        colorFuente = morado
-    elif colorFuente == "naranja" or colorFuente == "anaranjado":
-        colorFuente = naranja
-    elif colorFuente == "verde":
-        colorFuente = verde
-    elif colorFuente == "blanco":
-        colorFuente = blanco
-    elif colorFuente == "negro":
-        colorFuente = negro
+    if color_fuente_nodo== "rojo" or color_fuente_nodo=="red":
+        color_fuente_nodo="red"
+    elif color_fuente_nodo== "amarillo" or color_fuente_nodo=="yellow":
+        color_fuente_nodo= "yellow"
+    elif color_fuente_nodo== "azul" or color_fuente_nodo=="blue":
+        color_fuente_nodo= "blue"
+    elif color_fuente_nodo== "morado" or color_fuente_nodo=="purple":
+        color_fuente_nodo= "purple"
+    elif color_fuente_nodo== "naranja" or color_fuente_nodo== "anaranjado" or color_fuente_nodo=="orange":
+        color_fuente_nodo= "orange"
+    elif color_fuente_nodo== "verde" or color_fuente_nodo=="green":
+        color_fuente_nodo= "green"
+    elif color_fuente_nodo== "blanco" or color_fuente_nodo=="white":
+        color_fuente_nodo= "white"
+    elif color_fuente_nodo== "negro" or color_fuente_nodo=="black":
+        color_fuente_nodo= "black"
     else:
-        colorFuente = "grey"
+        color_fuente_nodo= "black"
     #Formas
-    if forma == "circulo" or forma == "circle":
-        forma = "circle"
-    elif forma == "cuadrado" or forma == "box":
-        forma = "box"
-    elif forma == "poligono" or forma == "polygon":
-        forma = "polygon"
-    elif forma == "elipse" or forma == "polygon":
-        forma = "ellipse"
-    elif forma == "triangulo" or forma == "triangle":
-        forma = "triangle"
-    elif forma == "ovalo" or forma == "oval":
-        forma = "oval"
-    elif forma == "rombo" or forma == "diamond":
-        forma = "diamond"
-    elif forma == "trapezoide" or forma == "trapezium":
-        forma = "trapezium"
+    if forma_nodo== "circulo" or forma_nodo== "circle":
+        forma_nodo= "circle"
+    elif forma_nodo == "cuadrado" or forma_nodo == "box":
+        forma_nodo = "box"
+    elif forma_nodo == "poligono" or forma_nodo == "polygon":
+        forma_nodo = "polygon"
+    elif forma_nodo == "elipse" or forma_nodo == "polygon":
+        forma_nodo = "ellipse"
+    elif forma_nodo == "triangulo" or forma_nodo == "triangle":
+        forma_nodo = "triangle"
+    elif forma_nodo == "ovalo" or forma_nodo == "oval":
+        forma_nodo = "oval"
+    elif forma_nodo == "rombo" or forma_nodo == "diamond":
+        forma_nodo = "diamond"
+    elif forma_nodo == "trapezoide" or forma_nodo == "trapezium":
+        forma_nodo = "trapezium"
     else:
-        forma = "circle"
+        forma_nodo = "box"
     dot = ""
     if objeto:
         if type(objeto) == Numero:
-            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.operar(None)}",fontcolor="{colorFuente}",fillcolor={colorFondo}, style=filled,shape={forma}];\n'
+            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.operar(None)}",fontcolor="{color_fuente_nodo}",fillcolor={color_fondo_nodo}, style=filled,shape={forma_nodo}];\n'
         if type(objeto) == operaciones_trigonometricas:
             print(objeto.columna)
-            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.tipo.lexema}\\n{objeto.operar(None)}",fontcolor="{colorFuente}",fillcolor={colorFondo}, style=filled,shape={forma}];\n'
-            dot += separar(i, id+1, etiqueta+"_angulo", objeto.left)
+            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.tipo.lexema}\\n{objeto.operar(None)}",fontcolor="{color_fuente_nodo}",fillcolor={color_fondo_nodo}, style=filled,shape={forma_nodo}];\n'
+            dot += configuracion_nodo(i, id+1, etiqueta+"_angulo", objeto.left)
             dot += f'nodo_{i}{id}{etiqueta} -> nodo_{i}{id+1}{etiqueta}_angulo;\n'
         if type(objeto) ==  operaciones_aritmeticas:
-            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.tipo.lexema}\\n{objeto.operar(None)}",fontcolor="{colorFuente}",fillcolor={colorFondo}, style=filled,shape={forma}];\n'
-            dot += separar(i, id+1, etiqueta + "_left", objeto.left)
+            dot += f'nodo_{i}{id}{etiqueta}[label="{objeto.tipo.lexema}\\n{objeto.operar(None)}",fontcolor="{color_fuente_nodo}",fillcolor={color_fondo_nodo}, style=filled,shape={forma_nodo}];\n'
+            dot += configuracion_nodo(i, id+1, etiqueta + "_left", objeto.left)
             dot += f'nodo_{i}{id}{etiqueta} -> nodo_{i}{id+1}{etiqueta}_left;\n'
-            dot += separar(i, id+1, etiqueta+"_right", objeto.right)
+            dot += configuracion_nodo(i, id+1, etiqueta+"_right", objeto.right)
             dot += f'nodo_{i}{id}{etiqueta} -> nodo_{i}{id+1}{etiqueta}_right;\n'
     return dot
 
